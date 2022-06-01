0fb2dc65abe2663fd7387819430f4b3526b5a6d9
YossraElhaddad <nspecial56@gmail.com>
2022-06-01 14:44:21 +0200

    Configure deployment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+/*module.exports = defineConfig({
   transpileDependencies: true
-})
+})*/
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/fe-intern-task/'
+    : '/',
+	defineConfig({
+  transpileDependencies: true
+  })
+}
